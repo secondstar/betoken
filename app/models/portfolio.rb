1cca667b6d2ea51c279845cf48461d6c9e60594f
Carl Tanner <carl@wdwhub.net>
2013-09-25 15:24:32 -0700

    added portfolio.rb model with spec.  Spec is only initial trial.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,11 @@
-class Portfolio < ActiveRecord::Base
+class Portfolio
+  attr_reader :undertaking
+  def title
+    "Doing Good Work"
+  end
+
+  def subtitle
+    "We designs and develops simple, intuitive web and mobile applications to help companies realize the power of social and business collaboration, cloud computing, and next-generation tools for the enterprise. Our developers specialize in Ruby, Ruby on Rails, HTML5,  and languages for iOS and Android mobile."
+  end
+  
 end
