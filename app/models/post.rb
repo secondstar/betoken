b205e2b7a8a746ce845326f03909459e7e21d398
Carl Tanner <carl@wdwhub.net>
2013-10-14 10:37:45 -0700

    partial refactor of blog.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,13 @@ class Post < ActiveRecord::Base
 
   # attr_accessible :title, :body, :image_url, :pubdate
 
+  composed_of :tags, :class_name => 'TagList', mapping: %w(tags, tags),
+                      :converter => ->(value) { TagList(value) }  
+
+  serialize :tags
   attr_accessor :blog
+  serialize   :tags
+  
 
   def self.most_recent(limit=10)
     order("pubdate DESC").limit(limit)
