b22a626a21f9be87bbac226e73f8efa60c577026
Carl Tanner <carl@wdwhub.net>
2013-11-07 17:07:43 -0800

    Added featured undertakings to different services pages.

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,30 +1,32 @@
 class ServicesController < ApplicationController
   def index
-    @undertakings = Project.most_recent(3)
+    featured_undertakings
     
   end
   
   def expertise
+    featured_undertakings
     
   end
   
   def design
-    
+    featured_undertakings
   end
   
   def development
-    
   end
   
   def mobile
-    
   end
   
   def training
-    
   end
   
   def how_we_work
-    
+  end
+  
+  private
+  def featured_undertakings
+    @undertakings = Project.most_recent(3)    
   end
 end
