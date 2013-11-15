abbbe710247654d486cd523f8e90375a5d029fca
Carl Tanner <carl@wdwhub.net>
2013-11-15 05:39:41 -0800

    start of refactor personalization of code to GuestSetting model

---
+++ app/models/guest_setting.rb
@@ -0,0 +1,51 @@
+class GuestSetting
+  def title
+    "Second Star Labs"
+  end
+  
+  # = {"name"=>"Second Star Labs", 
+  #   "email"=>"info@secondstarlabs.com", 
+  #   "phone"=>"801-499-4594", 
+  #   "twitter"=>"@secondstarlabs", 
+  #   "street"=>"328 Gateway Dr. #116", 
+  #   "city"=>"Pacifica", 
+  #   "state"=>"CA", 
+  #   "zip"=>"94044", 
+  #   "facebook"=>"", 
+  #   "linkedin"=>""}
+   def email
+     "info@secondstarlabs.com"
+   end
+   
+   def phone
+      ""
+   end
+   
+   def twitter
+    "@secondstarlabs"
+   end
+   
+   def street
+    "328 Gateway Dr. #116"
+   end
+   
+   def city
+    "Pacifica"
+   end
+   
+   def state
+    "CA"
+   end
+   
+   def zip
+    "94044"
+   end
+   
+   def facebook
+    ""
+   end
+   
+   def linkdin
+    ""
+   end
+end
